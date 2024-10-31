78b76c94701b17b9434bc64d5c87eb0daffe605e
CreateWithRup <soumajit19207@gmail.com>
2024-10-31 23:29:04 +0530

    updated page.js

--- app/(authentication)/sign-up/page.js
+++ app/(authentication)/sign-up/page.js
@@ -21,11 +21,11 @@ export default function Page() {
       <div className="h-[1px] bg-gray-300"></div>
 
       <main className="h-screen flex justify-center py-40">
-        <div className="flex flex-col md:items-center">
+        <div className="flex flex-col justify-center items-center">
           <Image className="w-64" src={devices} alt="tv" />
           <div className="space-y-1 mt-12 mb-12">
-            <p className="text-gray-800 md:text-center text-xs">STEP <strong>1</strong> OF <strong>4</strong></p>
-            <p className="text-gray-800 md:text-center text-3xl font-semibold w-2/3 mx-auto">Finish setting up your account</p>
+            <p className="text-gray-800 text-center text-xs">STEP <strong>1</strong> OF <strong>4</strong></p>
+            <p className="text-gray-800 text-center text-3xl font-semibold w-2/3 mx-auto">Finish setting up your account</p>
             <p className="text-gray-800 md:text-center w-1/2 mx-auto">Netflix is personalised for you. Create a password to watch on any device at any time.</p>
           </div>
           <Link href='/project' className="w-7/12 mx-auto bg-[#e50914] h-16 text-center pt-4 rounded-md text-2xl">
